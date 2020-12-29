3c8cb6ddf236d530efa905a1f0d2347a2ccaa7f7
qlenet2 <66741929+qlenet2@users.noreply.github.com>
2020-12-29 23:11:40 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -122,3 +122,27 @@ function fiveDayForecast(){
     });
     });
 }
+
+function checkAndDisplayValidationErrors(input) {
+    $('#errorMessages').empty();
+    
+    var errorMessages = [];
+    
+    input.each(function() {
+        if (!this.validity.valid) {
+            var errorField = $('label[for=' + this.id + ']').text();
+            errorMessages.push(errorField + ' ' + this.validationMessage);
+        }  
+    });
+    
+    if (errorMessages.length > 0){
+        $.each(errorMessages,function(index,message) {
+            $('#errorMessages').append($('<li>').attr({class: 'list-group-item list-group-item-danger'}).text(message));
+        });
+        // return true, indicating that there were errors
+        return true;
+    } else {
+        // return false, indicating that there were no errors
+        return false;
+    }
+}
