f4e2d59584af62e3a55b6bfd0af1a4871f793986
Cindy Okino <cindy.okino@gmail.com>
2020-12-29 17:54:21 -0500

    Add logit for 'Current Conditions'

--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
+var MY_WEATHER_API_KEY = 'a2b981f27643b4c19892f48e562a9d22';
+
+
 $(document).ready(function () {  
 
     requestButtonClick();
@@ -9,56 +12,71 @@ $(document).ready(function () {
 function requestButtonClick() {
     $('#requestButton').click(function (event) {
         var haveValidationErrors = checkAndDisplayValidationErrors($('#zipCode').find('input')); 
-        
+       
         if(haveValidationErrors) {
             return false;
         }
+    
+        clearCurrentConditions();
+        currentConditions();
+        fiveDayForecast();
     })
-
-    currentConditions();
-    fiveDayForecast();
 }
 
 
 // Function for Current Conditions in |City|
 function currentConditions() {
-    $('#requestButton').click(function (event) {
+    var zip = $("#zipCode").val(); 
+    var unit = $('#units').val();
+    var tempSymbol = " F";
+    var speedSymbol = " miles/hour";
 
-        $.ajax({
-           type: 'POST',
-           url: 'https://tsg-contactlist.herokuapp.com/contact',
-           data: JSON.stringify({
-                firstName: $('#addFirstName').val(),
-                lastName: $('#addLastName').val(),
-                company: $('#addCompany').val(),
-                phone: $('#addPhone').val(),
-                email: $('#addEmail').val()
-           }),
-           headers: {
-               'Accept': 'application/json',
-               'Content-Type': 'application/json'
-           },
-           'dataType': 'json',
-           success: function() {
-               $('#errorMessages').empty();
-               $('#addFirstName').val('');
-               $('#addLastName').val('');
-               $('#addCompany').val('');
-               $('#addphone').val('');
-               $('#addEmail').val('');
-               loadContacts();
-           },
-           error: function () {
-               $('#errorMessages')
-                .append($('<li>')
-                .attr({class: 'list-group-item list-group-item-danger'})
-                .text('Error calling web service. Please try again later.')); 
-           }
-        })
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&units=' + unit + '&appid=' + MY_WEATHER_API_KEY, 
+        
+        success: function(data) {
+            var img = data.weather[0].icon;
+            var weather = data.weather[0].main;
+            var description = data.weather[0].description;
+
+            if (unit == 'metric') {
+                tempSymbol = " °C";
+                speedSymbol = " km/h";
+            }
+                                   
+            $('#cityName').append(data.name); 
+            $('#leftDisplay').append('<img src=http://openweathermap.org/img/w/' + img + '.png width="80px">');
+            $('#leftDisplay').append(weather + ': ' + description);
+
+            $('#rightDisplay').append('<p>Temperature: ' + data.main.temp + tempSymbol + '</p>');
+            $('#rightDisplay').append('<p>Humidity: ' + data.main.humidity + ' %</p>');
+            $('#rightDisplay').append('<p>Wind: ' + data.wind.speed + speedSymbol + '</p>');
+        },
+
+        error: function () {
+            $('#errorMessages')
+            .append($('<li>')
+            .attr({class: 'list-group-item list-group-item-danger'})
+            .text('Error calling web service. Please try again later.')); 
+        }
     })
+    
+    $('#currentConditionsDiv').show();
 }
 
 
 // Function for Five Day Forecast
 function fiveDayForecast() {
 }
+
+
+// Function to clean Current Conditions Forecast data
+function clearCurrentConditions() {
+    $('#cityName').empty(); 
+    $('#leftDisplay').empty();
+    $('#rightDisplay').empty();
+}
+
+
+// second layer of validation to double-check the data and display the error messages with JavaScript
